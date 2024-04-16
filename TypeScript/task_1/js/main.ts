1d392405b6e52efa2c888f8a884581d38a9aadfc
TessierV <5628@holbertonstudents.com>
2024-04-16 06:49:24 -0700

    format

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,20 +1,18 @@
 export interface Teacher {
-	readonly firstName: string;
-	readonly lastName: string;
-	fullTimeEmployee: boolean;
-	yearsOfExperience?: number;
-	location: string;
-	[propName: string]: any;
-  }
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
 
-  const teacher3: Teacher = {
-	firstName: "John",
-	fullTimeEmployee: false,
-	lastName: "Doe",
-	location: "London",
-	contract: false,
-  };
+const teacher3: Teacher = {
+  firstName: "John",
+  fullTimeEmployee: false,
+  lastName: "Doe",
+  location: "London",
+  contract: false,
+};
 
-  console.log(teacher3);
-
-  console.log(teacher3); // La variable est bien orthographiée ici
+console.log(teacher3);
