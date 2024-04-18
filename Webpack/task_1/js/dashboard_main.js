261dfb85377ac990aed01d1223d5443d522e460f
TessierV <5628@holbertonstudents.com>
2024-04-18 14:59:04 -0700

    test

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,22 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-let count = 0;
+
 
 function updateCounter() {
-    count++;
+    let count = $('#count').text();
+    count = parseInt(count) + 1;
     $('#count').text(`${count} clicks on the button`);
 }
 
 const debouncedUpdateCounter = _.debounce(updateCounter, 500);
 
 $(document).ready(function () {
-    $('<p>').text('Holberton Dashboard').appendTo('body');
-    $('<p>').text('Dashboard data for the students').appendTo('body');
-    $('<button>').text('Click here to get started').click(debouncedUpdateCounter).appendTo('body');
-    $('<p>').attr('id', 'count').appendTo('body');
-    $('<p>').text('Copyright - Holberton School').appendTo('body');
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button id="start-btn">Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+
+    $('#start-btn').on('click', debounce(updateCounter, 500));
 });
