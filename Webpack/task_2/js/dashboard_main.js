9edfdc4a2f2cddd4f3b9851c2d1f73a0ac67508d
TessierV <5628@holbertonstudents.com>
2024-04-19 16:04:10 -0700

    test checker

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,18 +1,25 @@
-import $ from 'jquery';
-import _ from 'lodash';
-import '../css/main.css';
+const $ = require('jquery');
+const _ = require('lodash');
 
-$('body').append('<div id="logo"></div>');
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
+$('body').append(
+  $('<p>').text('Holberton Dashboard'),
+  $('<p>').text('Dashboard data for the students'),
+  $('<button>').text('Click here to get started'),
+  $('<p>').attr('id', 'count'),
+  $('<p>').text('Copyright - Holberton School'),
+)
+
+
 function updateCounter() {
-  count++;
-  $("#count").html(`${count} clicks on the button`);
-};
+  let countTag = $('#count');
+  let count = parseInt(countTag.attr('click_count')) || 0;
 
-$('button').on('click', _.debounce(updateCounter, 500));
+  $('button').click(function () {
+    countTag.text(`${++count} clicks on the button`);
+    countTag.attr('click_count', count);
+  });
+}
+
+_.debounce(updateCounter, 500);
+updateCounter();
