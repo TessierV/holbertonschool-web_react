672d7581c8a968d9dcfde92360e009f07d7ff588
TessierV <5628@holbertonstudents.com>
2024-04-20 02:19:47 -0700

    update from ex3

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,23 +1,32 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './js/dashboard_main.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["css-loader", "style-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassingOnDebug: true,
+              disable: true,
             },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                type: 'asset/resource',
-                use: ['file-loader', 'image-webpack-loader']
-            },
+          },
         ],
-    },
+      },
+    ],
+  },
 };
