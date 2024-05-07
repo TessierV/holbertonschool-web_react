3e53c5a14e83739e37f720bf01e9ef8b9f3c27c9
TessierV <5628@holbertonstudents.com>
2024-05-07 13:22:08 -0700

    test geolocalisation issu checker

--- React_inline_styling/task_1/dashboard/src/Login/Login.test.js
+++ React_inline_styling/task_1/dashboard/src/Login/Login.test.js
@@ -1,24 +1,25 @@
-import React from "react";
-import { shallow } from "enzyme";
-import Login from "./Login";
-import { StyleSheetTestUtils } from "aphrodite";
+import React from 'react';
+import { shallow } from 'enzyme';
+import Login from './Login';
+import { StyleSheetTestUtils } from 'aphrodite';
 
-describe("login component", () => {
-  beforeEach(() => {
+describe('Login Component', () => {
+  beforeAll(() => {
     StyleSheetTestUtils.suppressStyleInjection();
   });
 
-  afterEach(() => {
+  afterAll(() => {
     StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
   });
 
-  it("render without crashing", () => {
-    shallow(<Login />);
+  it('renders without crashing', () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.exists()).toBeTruthy();
   });
 
-  it("renders 2 input tags and 2 label tags", () => {
+  it('renders 2 input and 2 label tags', () => {
     const wrapper = shallow(<Login />);
-    expect(wrapper.find("input")).toHaveLength(2);
-    expect(wrapper.find("label")).toHaveLength(2);
+    expect(wrapper.find('input')).toHaveLength(2);
+    expect(wrapper.find('label')).toHaveLength(2);
   });
 });
--- React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
+++ React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
@@ -5,7 +5,6 @@ import NotificationItem from './NotificationItem';
 import NotificationItemShape from './NotificationItemShape';
 import { StyleSheet, css } from 'aphrodite';
 
-
 class Notifications extends Component {
   constructor(props) {
     super(props);
